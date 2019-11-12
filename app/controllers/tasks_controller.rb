2d903f16d1c73d20b7a34121fea8756689d8680e
Pierre Bedouret <pierrebedouret@hotmail.com>
2019-11-12 15:20:44 +0100

    Delete  = Done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,12 +14,28 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(tasks_params)
     @task.save
+    redirect_to tasks_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(tasks_params)
     redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
   end
 
   private
 
   def tasks_params
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
